b1e4798596bb146a120e1e55b2b27fafc80b88d1
harshitpatel123 <patelharshit123321@gmail.com>
2024-01-02 12:33:47 +0530

    adding git to react app folder

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import {Gl1} from './components/gallery';
+
+function App() {
+  return (
+    <>
+      <Navbar title="Harshit" aboutText="newabout" />
+      <Gl1/>
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/gallery.js
@@ -0,0 +1,16 @@
+export function Gl1(){
+    let name1 = "HarshitPatel"
+    return (
+        <div style={
+            {
+                backgroundColor: "yellow",
+                color: 'blue'
+            }
+        }>
+        <h1>This is Gallery componenet {name1}</h1>
+        <img src="\my-react-app\src\components\musicimg.jpg" alt="No media found" />
+        <img src="https://images.app.goo.gl/ie61raT5J5P4sY2p7" alt="No media found" />
+        <img src="musicimg.png" alt="No media found" />
+        </div>
+    )
+}
