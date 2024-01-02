e0bd9e2d68922a63cc99f6b69731e48818142556
harshitpatel123 <patelharshit123321@gmail.com>
2024-01-02 13:32:38 +0530

    added new file phtos.js

--- my-react-app/src/components/gallery.js
+++ my-react-app/src/components/gallery.js
@@ -1,3 +1,5 @@
+import Photo from "./photo"
+
 export function Gl1(){
     let name1 = "HarshitPatel"
     return (
@@ -8,9 +10,7 @@ export function Gl1(){
             }
         }>
         <h1>This is Gallery componenet {name1}</h1>
-        <img src="\my-react-app\src\components\musicimg.jpg" alt="No media found" />
-        <img src="https://images.app.goo.gl/ie61raT5J5P4sY2p7" alt="No media found" />
-        <img src="musicimg.png" alt="No media found" />
+            <Photo/>
         </div>
     )
 }
---
+++ my-react-app/src/components/photo.js
@@ -0,0 +1,9 @@
+export default function Photo(){
+    return (
+        <>
+        <img src="\my-react-app\src\components\musicimg.jpg" alt="No media found" />
+        <img src="https://images.app.goo.gl/ie61raT5J5P4sY2p7" alt="No media found" />
+        <img src="musicimg.png" alt="No media found" />
+        </>
+    )
+}
